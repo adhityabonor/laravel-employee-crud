3fd221b824d9936d80f94d50c539d66c11b48519
Adhitya Bonor <bonoradhitya@gmail.com>
2025-06-13 09:34:51 +0700

    Create Employee.php

---
+++ Employee.php
@@ -0,0 +1,23 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $table = 'employees';
+
+    protected $fillable = [
+        'nomor',
+        'nama',
+        'jabatan',
+        'talahir',
+        'photo_upload_path',
+        'created_on',
+        'updated_on',
+        'created_by',
+        'updated_by',
+        'deleted_on',
+    ];
+
+    public $timestamps = false;
+}
